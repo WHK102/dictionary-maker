452d492e6749c9379459fa1b9445fb7beef618b0
Yhojann <yan.uniko.102@gmail.com>
2019-09-03 04:21:51 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Dictionary maker
+
+Character combination file generator to be used in controlled security penetration tests.
+
+
+### Examples
+
+```
+$ ./maker.py 
+Dictionary Maker
+Enter max longitude of word [4]        : 3
+Use letters lowercase?      [Y/n]      : Y
+Use letters uppercase?      [y/N]      : N
+Use numbers?                [Y/n]      : N
+Use specials chars?         [y/N]      : N
+Filename of dictionary      [dict.txt] : 
+End!
+```
+
+It generate a dictionary file for for proof of concepts.
---
+++ maker.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+from pentest.brute.dict import BruteDictionaryIter
+from pentest.io         import Inputs
+
+
+class Controller(object):
+
+    def __init__(self):
+
+
+        # Header message
+        print('Dictionary Maker');
+
+        inputs = Inputs()
+
+        data = {
+            'max-length' : inputs.get('Enter max longitude of word [4]        : ', int, 4),
+            'lowercase'  : inputs.get('Use letters lowercase?      [Y/n]      : ', bool, True),
+            'uppercase'  : inputs.get('Use letters uppercase?      [y/N]      : ', bool, False),
+            'numbers'    : inputs.get('Use numbers?                [Y/n]      : ', bool, True),
+            'specials'   : inputs.get('Use specials chars?         [y/N]      : ', bool, False),
+            'filename'   : None
+        }
+
+        while(True):
+
+            # Get the filename of dictionary to make
+            data['filename'] = inputs.get('Filename of dictionary      [dict.txt] : ', str, 'dict.txt')
+
+            # File exist?
+            if(not os.path.isfile(data['filename'])):
+                break
+
+            # Replace the file?
+            if(inputs.get('The file exists. You want to replace it? [y/N] : ', bool, False)):
+
+                # Delete old file
+                os.remove(data['filename'])
+
+                break
+
+        # File dictionary pointer on memory
+        fileHandler = open(data['filename'], 'w')
+
+        # Make the iterator
+        bruteDictionaryIter = BruteDictionaryIter(
+            maxLength = data['max-length'],
+            lowercase = data['lowercase'],
+            uppercase = data['uppercase'],
+            numbers   = data['numbers'],
+            specials  = data['specials']
+        )
+
+        # Process the iteration
+        for item in bruteDictionaryIter:
+
+            # Write the word
+            fileHandler.write(item + '\n')
+
+            # Print the progress            
+            print(''.join([
+                '\r',
+                'Progress: ',
+                str(bruteDictionaryIter.currentWordIndex),
+                '/',
+                str(bruteDictionaryIter.totalWords),
+                ' - ',
+                item
+            ]), end='', flush=True)
+
+        fileHandler.close()
+
+        print('\r                                                       \rEnd!')
+
+
+if __name__ == '__main__':
+
+    try:
+        Controller()
+
+    except KeyboardInterrupt as e:
+        # Ctrl+C, it's ok.
+        pass
+
+    except Exception as e:
+        # Unhandled exception
+        raise e
